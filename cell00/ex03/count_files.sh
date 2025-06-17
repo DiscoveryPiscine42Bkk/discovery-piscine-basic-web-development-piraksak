e6862954f2f8f24a2af5551acf72b61e4423a278
piraksak <pichaya.raksakom2205@gmail.com>
2025-06-17 07:25:25 +0000

    sybau

--- cell00/ex03/count_files.sh
+++ cell00/ex03/count_files.sh
@@ -1,9 +1,1 @@
- <td>
-  <strong>What this cell will show you:</strong>
-  <ul>
-    <li>Discover the terminal and the command line.</li>
-    <li>First commands to navigate, modify and create in the filesystem.</li>
-    <li>First programs to automate some tasks in your terminal.</li>
-  </ul>
-</td>
- 
+ 42
